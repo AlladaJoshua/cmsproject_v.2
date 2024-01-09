62356ce6bbaab701919530c1517a1842492336e9
joshua allada <allada.personal@gmail.com>
2024-01-09 15:13:51 +0800

    Certificate view and download btns and responsiveness

--- src/Components/Content.jsx
+++ src/Components/Content.jsx
@@ -1,11 +1,55 @@
 import React from "react";
 import "../Css/content.css";
+import cert from "../assets/cert.png";
+
 const Content = () => {
   return (
     <div>
       <section className="content">
         <h1>Certification</h1>
         <div className="hr"></div>
+      </section>
+      <section className="certificates">
+        <div className="cert">
+          <img src={cert} alt="certificate" />
+          <p>Course Title</p>
+          <div className="overlay">
+            <div className="buttons">
+              <button className="view">View</button>
+              <button className="download">Dowload</button>
+            </div>
+          </div>
+        </div>
+        <div className="cert">
+          <img src={cert} alt="certificate" />
+          <p>Course Title</p>
+          <div className="overlay">
+            <div className="buttons">
+              <button className="view">View</button>
+              <button className="download">Dowload</button>
+            </div>
+          </div>
+        </div>
+        <div className="cert">
+          <img src={cert} alt="certificate" />
+          <p>Course Title</p>
+          <div className="overlay">
+            <div className="buttons">
+              <button className="view">View</button>
+              <button className="download">Dowload</button>
+            </div>
+          </div>
+        </div>
+        <div className="cert">
+          <img src={cert} alt="certificate" />
+          <p>Course Title</p>
+          <div className="overlay">
+            <div className="buttons">
+              <button className="view">View</button>
+              <button className="download">Dowload</button>
+            </div>
+          </div>
+        </div>
       </section>
     </div>
   );
